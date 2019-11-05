0039bf22f04e8aaad7f70ee992ad18edeb01adb6
Loly-webdev <eloise.ruiz.rodriguez@gmail.com>
2019-11-05 16:15:29 +0100

    add include and dynamic page

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,10 +1,8 @@
-<?php
-?>
 <p>
     <strong> © Eloïse RUIZ-RODRIGUEZ | 2019</strong><br>
     Dans le cadre de ma formation avec OpenClassrooms.
 </p>
 <hr>
 <p>
-    <a href="pages/connexion.html">Connexion</a>
+    <a href="../pages/connexion.php">Connexion</a>
 </p>
--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<?php
-?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -12,39 +10,7 @@
 </header>
 <!-- Contenu du corps de la page-->
 <main id="home">
-    <!-- Presentation -->
-    <section id="about">
-        <img alt="Eloïse RUIZ-RODRIGUEZ" src="images/profile.png">
-        <article>
-            <h1>Eloïse RUIZ-RODRIGUEZ</h1>
-            <h2>Développeur / Intégrateur Web</h2>
-            <p>
-                Passionnée par le milieu informatique et plus particulièrement par le développement web,
-                j’ai entrepris une formation de<br>
-                « Développeur d'application PHP/Symfony », en collaboration avec
-                <a href="https://openclassrooms.com" target="_blank">OpenClassrooms.</a><br>
-                Aujourd'hui je vous présente mon blog professionnel, qui je l'éspère sera à votre goût.<br>
-            </p>
-        </article>
-    </section>
-    <!-- CV -->
-    <section id="cv">
-        <h3>Télécharger mon CV :</h3>
-        <div>
-            <a href="docs/CV_Eloïse_RUIZ-RODRIGUEZ.pdf" target="_blank">
-                Télécharger mon CV</a>
-        </div>
-    </section>
-    <!-- Social -->
-    <section id="social">
-        <h3>Suivez-moi :</h3>
-        <div>
-            <a href="https://www.linkedin.com/in/eloïse-ruiz-rodriguez" target="_blank">
-                <img alt="icone linkedin" src="images/ico_in.png"></a>
-            <a href="https://github.com/Loly-webdev" target="_blank">
-                <img alt="icone github" src="images/ico_git.png"></a>
-        </div>
-    </section>
+    <?php include("includes/_main.php"); ?>
 </main>
 <!-- Footer -->
 <footer>
