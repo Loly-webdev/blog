39e29263756d2172465d13521e789965c82e5187
Loly-webdev <eloise.ruiz.rodriguez@gmail.com>
2019-11-06 20:21:12 +0100

    modification include to require

--- includes/_nav.php
+++ includes/_nav.php
@@ -1,9 +1,9 @@
 <nav class="flex">
     <a href="../index.php?page=home"><img alt="logo du blog Eloïse" src="../images/logo_blanc.png" href="../index.php?page=home"></a>
     <ul class="flex">
-        <li><a href="../index.php?page=home">Accueil</a></li>
-        <li><a href="../index.php?page=blog">Blog</a></li>
-        <li><a href="../index.php?page=contact">Contact</a></li>
-        <li><a href="../index.php?page=connexion">Connexion</a></li>
+        <li><a href="index.php?page=home">Accueil</a></li>
+        <li><a href="index.php?page=blog">Blog</a></li>
+        <li><a href="index.php?page=contact">Contact</a></li>
+        <li><a href="index.php?page=connexion">Connexion</a></li>
     </ul>
 </nav>
--- index.php
+++ index.php
@@ -2,19 +2,42 @@
 <html lang="fr">
 <head>
     <title>accueil</title>
-    <?php include("includes/_head.php"); ?>
+    <?php require("includes/_head.php"); ?>
 </head>
 <body>
 <header>
-    <?php include("includes/_nav.php"); ?>
+    <?php require("includes/_nav.php"); ?>
 </header>
 <!-- Contenu du corps de la page-->
 <main id="home">
-    <?php include("includes/_main.php"); ?>
+    <?php
+    // Place the value from ?page=value in the URL to the variable $page.
+    $page = $_GET['page'];
+
+    // Create an array of the only pages allowed.
+    $pageArray = array(
+        'home',
+        'blog',
+        'contact',
+        'connexion',
+        'comment',
+        'post'
+    );
+
+    // Is $page in the array?
+    $inArray = in_array($page, $pageArray);
+
+    // If so, include it, if not, emit error.
+    if ($inArray == true) {
+        require('pages/'. $page .'.php');
+    } else {
+        require('pages/home.php');
+    }
+    ?>
 </main>
 <!-- Footer -->
 <footer>
-    <?php include("includes/_footer.php"); ?>
+    <?php require("includes/_footer.php"); ?>
 </footer>
 </body>
 </html>
