a48ec1912ac3c98c6dbd30fb59ec9343f34f7e3d
Loly-webdev <eloise.ruiz.rodriguez@gmail.com>
2019-11-08 15:31:06 +0100

    modification controllers and views

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
-require('controller/HomeController.php');
-
-require('controller/PostController.php');
-
-require('controller/BlogController.php');
 
 require('core/DefaultController.php');
+
+// Place the value from ?page=value in the URL to the variable $page.
+$server = $_SERVER["REQUEST_URI"];
+$serverData = trim(parse_url($server, PHP_URL_PATH), "/");
+$params = explode('/', $serverData);
+$page = reset($params);
+if ('' != $page) {
+    require('view/frontend/' . $page . '.php');
+} else {
+    require('view/frontend/homeView.php');
+}
+
+//$controllerName = $params[0] ?? 'home';
+//$methodeName = $params[1] ?? 'index';
+
+//$controllerName = null;
+//if (isset($params[0])) {
+//    $controllerName = $params[0];
+//}
