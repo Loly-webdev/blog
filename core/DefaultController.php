bb5399eb7296aba7a825d3dc3d45237ecb2b4d8c
Loly-webdev <eloise.ruiz.rodriguez@gmail.com>
2019-11-08 16:47:51 +0100

    modification router

--- core/DefaultController.php
+++ core/DefaultController.php
@@ -5,3 +5,33 @@ abstract class DefaultController
     abstract function index();
 }
 
+$server = $_SERVER["REQUEST_URI"];
+$serverData = trim(parse_url($uri, PHP_URL_PATH),"/");
+$params = explode('/', $uri);
+$controllerName = $params[0] ?? 'home';
+$methodeName = $params[1] ?? 'index';
+
+$controllerName = null;
+if (isset($params[0])) {
+    $controllerName = $params[0];
+}
+
+class HomeController {
+    public function index()
+    {
+
+    }
+}
+
+class PostsController {
+    public function shaw()
+    {
+        $articleId = $_GET['id'];
+    }
+
+    public function index()
+    {
+
+    }
+}
+
