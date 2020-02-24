c12d911d8959a72f59b65e598d001d840dd1d295
Loly-webdev <eloise.ruiz.rodriguez@gmail.com>
2020-02-24 18:26:29 +0100

    correction code and start feature

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Core\DefaultAbstractController;
+
+class ErrorController extends DefaultAbstractController
+{
+    public function indexAction()
+    {
+    }
+
+    public function error($t)
+    {
+        $this->renderView(
+            '../error.html.twig',
+            [
+                'message' => $t->getMessage(),
+                'file' => $t->getFile(),
+                'line' => $t->getLine()
+            ]
+        );
+    }
+}
