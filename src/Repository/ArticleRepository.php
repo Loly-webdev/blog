c959e44622140a2ed704c5794e1f692e2d09dd9e
loly-webdev <eloise.ruiz.rodriguez@gmail.com>
2020-03-12 18:06:23 +0100

    correction syntax and start method update

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -17,22 +17,12 @@ class ArticleRepository extends DefaultAbstractRepository
 
     public function add($data)
     {
-        //faire le $sql en dynamique
-        //array_keys();
-        //implode(', ');
-        //if pas id faire insert into
-        //if id update
-        //faire methode save qui soit fait insert soit update
-
-        //$sql = 'INSERT INTO ' . static::$tableName . ' (title, author, content)
-        // VALUES (:title, :author, :content)';
-
         $key = array_keys($data);
         $val = array_values($data);
-
         $table = static::$tableName;
-        $sql = "INSERT INTO $table (" . implode(', ', $key) . ") "
-               . "VALUES ('" . implode("', '", $val) . "')";
+
+        $sql   = "INSERT INTO $table (" . implode(', ', $key) . ") "
+                 . "VALUES ('" . implode("', '", $val) . "')";
 
         $pdo = $this->getPDO()->prepare($sql);
         $pdo->execute($data);
@@ -41,15 +31,17 @@ class ArticleRepository extends DefaultAbstractRepository
         return $pdo->rowCount();
     }
 
-    public function update($id): int
+    public function update($data)
     {
-        $sql = 'UPDATE ' . static::$tableName
-               . ' SET title = :title, content = :content '
-               . ' WHERE ' . static::$tablePk . ' = ?';
+        $key = array_keys($data);
+        $val = array_values($data);
+        $table = static::$tableName;
+
+        $sql   = "UPDATE $table (" . implode(', ', $key) . ") "
+                 . "VALUES ('" . implode("', '", $val) . "')";
 
         $pdo = $this->getPDO()->prepare($sql);
-        $pdo->setFetchMode(PDO::FETCH_CLASS, $this->getEntity());
-        $pdo->execute([$id]);
+        $pdo->execute($data);
 
         // The number of updated entries is displayed.
         return $pdo->rowCount();
