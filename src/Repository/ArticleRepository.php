73120363b6872c2f225f09d09e9c11b71336e2bb
loly-webdev <eloise.ruiz.rodriguez@gmail.com>
2020-03-18 15:40:00 +0100

    correction and modification method add, view and BDD

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -3,9 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Article;
-use Core\DefaultAbstractEntity;
 use Core\DefaultAbstractRepository;
-use PDO;
 
 /**
  * Make the database requests relative to the articles
@@ -13,39 +11,6 @@ use PDO;
 class ArticleRepository extends DefaultAbstractRepository
 {
     static $tableName  = 'posts';
-    static $tableOrder = 'creation_date';
-
-    public function add($data)
-    {
-        $key = array_keys($data);
-        $val = array_values($data);
-        $table = static::$tableName;
-
-        $sql   = "INSERT INTO $table (" . implode(', ', $key) . ") "
-                 . "VALUES ('" . implode("', '", $val) . "')";
-
-        $pdo = $this->getPDO()->prepare($sql);
-        $pdo->execute($data);
-
-        // The number of updated entries is displayed.
-        return $pdo->rowCount();
-    }
-
-    public function update($data)
-    {
-        $key = array_keys($data);
-        $val = array_values($data);
-        $table = static::$tableName;
-
-        $sql   = "UPDATE $table (" . implode(', ', $key) . ") "
-                 . "SET ('" . implode("', '", $val) . "')";
-
-        $pdo = $this->getPDO()->prepare($sql);
-        $pdo->execute($data);
-
-        // The number of updated entries is displayed.
-        return $pdo->rowCount();
-    }
 
     public function getEntity()
     {
--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -4,7 +4,6 @@ namespace App\Repository;
 
 use Core\DefaultAbstractRepository;
 use App\Entity\Comment;
-use PDO;
 
 /**
  * Make the database requests relative to the comments
@@ -12,26 +11,6 @@ use PDO;
 class CommentRepository extends DefaultAbstractRepository
 {
     static $tableName  = 'comments';
-    static $tableOrder = 'comment_date';
-
-    public function add(array $data)
-    {
-        $sql = 'INSERT INTO ' . static::$tableName . ' (post_id, author, content)
-            VALUES (:post_id, :author, :content)';
-
-        $pdo = $this->getPDO()->prepare($sql);
-
-        $pdo->execute(
-            [
-                'post_id' => (int)$_GET['articleId'],
-                'author'  => $data['author'],
-                'content' => $data['content']
-            ]
-        );
-
-        // The number of updated entries is displayed.
-        return $pdo->rowCount();
-    }
 
     public function getEntity()
     {
