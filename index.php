bf62f0d00cf748b23ea848547795bf418c7d261c
loly-webdev <eloise.ruiz.rodriguez@gmail.com>
2020-03-21 18:50:43 +0100

    test trait delete and correction method getTwig

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 
-
-define('DEBUG_TWIG', true);
-
 // Load autoload of composer
 require 'vendor/autoload.php';
 
@@ -13,9 +10,8 @@ use App\Controller\ErrorController;
 use Core\Dispatcher;
 
 try {
-	$dispatcher = new Dispatcher();
-	$dispatcher->dispatch();
-
+    $dispatcher = new Dispatcher();
+    $dispatcher->dispatch();
 } catch (Throwable $t) {
     $error = new ErrorController();
     $error->error($t);
