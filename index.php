d62cc64636c6f21450335e926fd3abb411327ba9
loly-webdev <eloise.ruiz.rodriguez@gmail.com>
2020-03-24 14:26:06 +0100

    change errorController by errors Management and add errorsPage

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php
 // Show PHP errors (Delete before prod)
 error_reporting(E_ALL | E_STRICT);
+//error_reporting(0);
 ini_set('display_errors', 1);
+require_once ('errors/errorsManagement.php');
+set_error_handler('errorsManagement');
 
 // Load autoload of composer
 require 'vendor/autoload.php';
 
-use App\Controller\ErrorController;
 use Core\Dispatcher;
 
 try {
     $dispatcher = new Dispatcher();
     $dispatcher->dispatch();
 } catch (Throwable $t) {
-    $error = new ErrorController();
-    $error->error($t);
+    $type = $t->getCode();
+    $message = $t->getMessage();
+    $file = $t->getFile();
+    $line = $t->getLine();
+    require_once('template/errors/errorsManagementView.php');
 }
