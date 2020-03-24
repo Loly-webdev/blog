143d3e6dee25b6cdec2cc5fd47b6742c3d349fdc
loly-webdev <eloise.ruiz.rodriguez@gmail.com>
2020-03-24 15:22:12 +0100

    correction errorsPages

--- template/errors/errorsPageView.php
+++ template/errors/errorsPageView.php
@@ -5,18 +5,17 @@
     <!-- Favicon -->
     <link href="/public/images/favicon.png" rel="icon" type="image/png"/>
     <!-- Css files -->
-    <link href="/public/css/style.css" rel="stylesheet"/>
+    <link href="/public/css/styleError.css" rel="stylesheet"/>
     <!--Viewport -->
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <!-- Google font -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900" rel="stylesheet">
     <title>Error <?= $errorCode ?></title>
 </head>
-<body>
-<main>
+<body class="error" >
     <section id="notfound">
         <div class="notfound">
-            <div class="notfound-404">
+            <div class="notfound-page">
                 <h1>Oops!</h1>
             </div>
             <h2><?= $errorCode . ' - ' . $errorMessage ?></h2>
@@ -24,6 +23,5 @@
             <a href="/">Revenir à l'accueil</a>
         </div>
     </section>
-</main>
 </body>
 </html>
