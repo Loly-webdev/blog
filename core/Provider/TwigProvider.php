2cfe4c218affb2b490dc38fc9239b6ded80f6042
loly-webdev <eloise.ruiz.rodriguez@gmail.com>
2020-03-28 19:10:37 +0100

    correction to connection database(directory config)

--- core/Provider/TwigProvider.php
+++ core/Provider/TwigProvider.php
@@ -2,6 +2,8 @@
 
 namespace Core\Provider;
 
+use Config\Configuration;
+use Exception;
 use Twig\Environment;
 use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
@@ -18,18 +20,22 @@ class TwigProvider
     /**
      * Instance of Twig
      * @return mixed
+     * @throws Exception
      */
     public static function getTwig()
     {
-        $debug = true;
+        $config = Configuration::getInstance();
+        $debug = $config->getTwigConfig();
+
         if (null === static::$twig) {
             $loader       = new FilesystemLoader('template/');
-            static::$twig = new Environment($loader,
-                // To the prod define the path of directory Cache, else to dev keep false
-                                            [
-                                                'cache' => false,
-                                                'debug' => $debug //or true
-                                            ]
+            static::$twig = new Environment(
+                $loader,
+             // To the prod define the path of directory Cache, else to dev keep false
+                [
+                    'cache' => false,
+                    'debug' => $debug
+                ]
             );
         }
 
