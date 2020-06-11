f3498807b0187d4eedcc5ce71f783a4c484be5c8
Loly-webdev <eloise.ruiz.rodriguez@gmail.com>
2020-06-11 15:53:01 +0200

    modification contact controller

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -20,24 +20,19 @@ class ContactController extends DefaultAbstractController
      */
     public function indexAction()
     {
-        $this->mailAction();
-    }
-
-    public function mailAction()
-    {
         $entityName = 'contact';
 
         if (isset($_SESSION['logged']) === true) {
-            $userId = $_SESSION['user'];
-            $user   = (new UserRepository())->findOneById($userId);
-            $name   = $user->getLogin();
-            $mail   = $user->getMail();
+            $userId   = $_SESSION['user'];
+            $user     = (new UserRepository())->findOneById($userId);
+            $name = $user->getLogin();
+            $mail = $user->getMail();
         }
 
         if ($this->hasFormSubmitted($entityName)) {
             $myMail   = "lolywebdev@gmail.com";
             $formData = $this->getFormSubmittedValues($entityName);
-            $name     = $formData['name'];
+            $name     = $formData['name'] ? $this->rec($formData['name']) : '';
             $mail     = $formData['email'];
 
             //on vérifie que l'adresse est correcte
@@ -50,16 +45,17 @@ class ContactController extends DefaultAbstractController
             //on renseigne les entêtes de la fonction mail de PHP
             $header = "MIME-Version: 1.0\r\n";
             $header .= "Content-type: text/html; charset=UTF-8\r\n";
-            $header .= "From: MESSAGE DU SITE LOLYWEBDEV <$mail>\r\n";
-            $header .= "Reply-To: $name <$mail>\r\n";
+            $header .= "From: $mail\r\n";
+            $header .= "Reply-To: $mail\r\n";
 
             //on prépare les champs:
-            $subject        = htmlentities($formData['subject'], ENT_QUOTES, "UTF-8");
-            $messageContent = htmlentities($formData['message'], ENT_QUOTES, "UTF-8");
+            $subject        = $formData['subject'] ? $this->rec($formData['subject']) : '';
+            $messageContent = "<h2>MESSAGE DU SITE LOLYWEBDEV de $name, $mail</h2>"
+                . $formData['message'] ? $this->rec($formData['message']) : '';
 
             //en fin, on envoi le mail
-            dump($myMail, $subject, nl2br($messageContent), $header);
-            if (mail($myMail, $subject, nl2br($messageContent), $header)) {
+            dump(mail($myMail, $subject, $messageContent, $header),$myMail, $subject, $messageContent, $header);
+            if (mail($myMail, $subject, $messageContent, $header)) {
                 $message = "Le mail à été envoyé avec succès!";
             }
             $message = "Une erreur est survenue, le mail n'a pas été envoyé";
@@ -73,5 +69,13 @@ class ContactController extends DefaultAbstractController
                 'message'      => $message ?? ''
             ]
         );
+    }
+
+    function Rec($text)
+    {
+        $text = htmlspecialchars(trim($text), ENT_QUOTES);
+        $text = nl2br($text);
+
+        return $text;
     }
 }
