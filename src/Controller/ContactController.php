c07c8f2d89bb85a549ebd2f96b76304d7ae24edc
LolyWD <eloise.ruiz.rodriguez@gmail.com>
2020-07-03 16:47:56 +0200

    refactorisation ContactController

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -23,15 +23,10 @@ class ContactController extends DefaultAbstractController
      */
     public function indexAction()
     {
-        $viewFolder = 'front/';
-
         if (isset($_SESSION['logged']) === true) {
             $viewFolder = 'back/';
-
-            $userId = $_SESSION['id'];
-            $user   = (new UserRepository())->findOneById($userId);
-            assert($user instanceof User);
-            $nameUser  = $user->getLogin();
+            $user = $this->contactLogged();
+            $nameUser = $user->getLogin();
             $emailUser = $user->getMail();
         }
 
@@ -40,37 +35,66 @@ class ContactController extends DefaultAbstractController
         if ($formValidator->isSubmitted() && $formValidator->isValid()) {
 
             $formValues = $formValidator->getFormValues();
-            $nameUser   = $formValues['nameUser'] ? Helper::verifyText($formValues['nameUser']) : '';
-            $emailUser  = $formValues['email'] ?? '';
+            $nameUser = $formValues['nameUser'] ? Helper::verifyText($formValues['nameUser']) : '';
+            $emailUser = $formValues['email'] ?? '';
+
 
             if (false === Helper::verifyAddress($emailUser)) {
                 throw new Exception("l'adresse $emailUser n'est pas valide");
             }
 
-            // We prepare the fields
-            $subject        = $formValues['subject'] ? Helper::verifyText($formValues['subject']) : '';
-            $messageContent = $formValues['message'] ? Helper::verifyText($formValues['message']) : '';
-            $message        = "MESSAGE DU SITE LOLYWEBDEV de $nameUser, $emailUser\r\n"
-                              . $messageContent;
+            $fields = $this->prepareFields($formValues, $nameUser, $emailUser);
 
-            $status        = "danger";
-            $statusMessage = "une erreur est survenue, le mail n'a pas pu être envoyé";
-
-            if (true === (new Email())->sendMail($emailUser, $subject, $message)) {
-                $status        = "success";
-                $statusMessage = "Le mail à été envoyé avec succès";
-            }
+            $status = $this->status($emailUser, $fields['subject'], $fields['message']);
         }
 
         $this->renderView(
             'contact.html.twig',
             [
-                'nameUser'      => $nameUser ?? '',
-                'email'         => $emailUser ?? '',
-                'status'        => $status ?? '',
-                'statusMessage' => $statusMessage ?? ''
+                'nameUser' => $nameUser ?? '',
+                'email' => $emailUser ?? '',
+                'status' => $status['status'] ?? '',
+                'statusMessage' => $status['statusMessage'] ?? ''
             ],
-            $viewFolder
+            $viewFolder ?? 'front/'
         );
+    }
+
+    public function contactLogged(): User
+    {
+        $userId = $_SESSION['id'];
+        $user = (new UserRepository())->findOneById($userId);
+        assert($user instanceof User);
+
+        return $user;
+    }
+
+    public function status($emailUser, $subject, $message): array
+    {
+        $status = "danger";
+        $statusMessage = "une erreur est survenue, le mail n'a pas pu être envoyé";
+
+        if (true === (new Email())->sendMail($emailUser, $subject, $message)) {
+            $status = "success";
+            $statusMessage = "Le mail à été envoyé avec succès";
+        }
+
+        return [
+            'status' => $status ?? '',
+            'statusMessage' => $statusMessage ?? ''
+        ];
+    }
+
+    public function prepareFields($formValues, $nameUser, $emailUser): array
+    {
+        $subject = $formValues['subject'] ? Helper::verifyText($formValues['subject']) : '';
+        $messageContent = $formValues['message'] ? Helper::verifyText($formValues['message']) : '';
+        $message = "MESSAGE DU SITE LOLYWEBDEV de $nameUser, $emailUser\r\n"
+            . $messageContent;
+
+        return [
+            'subject' => $subject,
+            'message' => $message
+        ];
     }
 }
