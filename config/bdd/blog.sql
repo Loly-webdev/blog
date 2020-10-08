8c27ad4b8e4fd6580a59399356b1c8c05b9c2730
LolyWd <eloise.ruiz.rodriguez@gmail.com>
2020-10-08 09:36:33 +0200

    add database

--- config/bdd/blog.sql
+++ config/bdd/blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 20 juil. 2020 à 08:26
+-- Généré le :  jeu. 08 oct. 2020 à 07:34
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.4.0
 
@@ -13,17 +13,14 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `blog`
 --
-DROP DATABASE IF EXISTS `blog`;
-CREATE DATABASE IF NOT EXISTS `blog` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `blog`;
 
 -- --------------------------------------------------------
 
@@ -32,29 +29,25 @@ USE `blog`;
 --
 
 DROP TABLE IF EXISTS `article`;
-CREATE TABLE IF NOT EXISTS `article`
-(
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `title` varchar(255) NOT NULL,
-    `author` varchar(30) NOT NULL,
-    `content` text NOT NULL,
-    `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
-    `updatedAt` datetime NOT NULL DEFAULT current_timestamp(),
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 5 DEFAULT CHARSET = utf8;
+CREATE TABLE IF NOT EXISTS `article` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(30) NOT NULL,
+  `hat` text NOT NULL,
+  `content` text NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `article`
 --
 
-INSERT INTO `article` (`id`, `title`, `author`, `content`, `createdAt`, `updatedAt`)
-VALUES (1, 'Bienvenue sur mon blog !', '',
-        'Je vous souhaite à toutes et à tous la bienvenue sur mon blog qui parlera de... PHP bien sûr !',
-        '2010-03-25 16:28:41', '2020-03-18 11:06:28'),
-       (2, 'Le PHP à la conquête du monde !', '',
-        'C\'est officiel, l\'éléPHPant a annoncé à la radio hier soir \"J\'ai l\'intention de conquérir le monde !\".\r\nIl a en outre précisé que le monde serait à sa botte en moins de temps qu\'il n\'en fallait pour dire \"éléPHPant\". Pas dur, ceci dit entre nous...',
-        '2010-03-27 18:31:11', '2020-03-18 11:06:28'),
-       (3, 'test', 'Loly', 'test', '2020-06-03 11:51:04', '2020-06-03 11:51:04');
+INSERT INTO `article` (`id`, `title`, `author`, `hat`, `content`, `createdAt`, `updatedAt`) VALUES
+(1, 'Bienvenue sur mon blog !', 'LolyWD', 'Bienvenue sur mon blog !', 'Je vous souhaite à toutes et à tous la bienvenue sur mon blog qui parlera de... PHP bien sûr !', '2010-03-25 16:28:41', '2020-03-18 11:06:28'),
+(2, 'Le PHP à la conquête du monde !', 'Nowhere', 'L\'éléphant l\'a annoncé, il a l\'intention de conquérir le monde !', 'C\'est officiel, l\'éléPHPant a annoncé à la radio hier soir \"J\'ai l\'intention de conquérir le monde !\".\r\nIl a en outre précisé que le monde serait à sa botte en moins de temps qu\'il n\'en fallait pour dire \"éléPHPant\". Pas dur, ceci dit entre nous...', '2010-03-27 18:31:11', '2020-03-18 11:06:28'),
+(11, 'test', 'Loly', 'test hat', 'test content', '2020-07-24 21:27:04', '2020-08-27 15:10:18');
 
 -- --------------------------------------------------------
 
@@ -63,30 +56,30 @@ VALUES (1, 'Bienvenue sur mon blog !', '',
 --
 
 DROP TABLE IF EXISTS `comment`;
-CREATE TABLE IF NOT EXISTS `comment`
-(
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `articleId` int(11) NOT NULL,
-    `author` varchar(255) NOT NULL,
-    `content` text NOT NULL,
-    `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
-    `updatedAt` datetime NOT NULL DEFAULT current_timestamp(),
-    PRIMARY KEY (`id`),
-    KEY `articleId` (`articleId`)
-) ENGINE = InnoDB AUTO_INCREMENT = 7 DEFAULT CHARSET = utf8;
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `approved` varchar(3) NOT NULL,
+  `articleId` int(11) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `articleId` (`articleId`)
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `comment`
 --
 
-INSERT INTO `comment` (`id`, `articleId`, `author`, `content`, `createdAt`, `updatedAt`)
-VALUES (1, 1, 'M@teo21', 'Un peu court ce billet !', '2010-03-25 16:49:53', '2020-03-18 11:05:41'),
-       (2, 1, 'Maxime', 'Oui, ça commence pas très fort ce blog...', '2010-03-25 16:57:16', '2020-03-18 11:05:41'),
-       (3, 1, 'MultiKiller', '+1 !', '2010-03-25 17:12:52', '2020-03-18 11:05:41'),
-       (4, 2, 'John', 'Preum\'s !', '2010-03-27 18:59:49', '2020-03-18 11:05:41'),
-       (5, 2, 'Maxime', 'Excellente analyse de la situation !\r\nIl y arrivera plus tôt qu\'on ne le pense !',
-        '2010-03-27 22:02:13', '2020-03-18 11:05:41'),
-       (6, 4, 'loly', 'test', '2020-06-03 11:51:17', '2020-06-03 11:51:17');
+INSERT INTO `comment` (`id`, `approved`, `articleId`, `author`, `content`, `createdAt`, `updatedAt`) VALUES
+(1, 'oui', 1, 'M@teo21', 'Un peu court ce billet !', '2010-03-25 16:49:53', '2020-03-18 11:05:41'),
+(2, 'oui', 1, 'Maxime', 'Oui, ça commence pas très fort ce blog...', '2010-03-25 16:57:16', '2020-03-18 11:05:41'),
+(3, 'oui', 1, 'MultiKiller', '+1 !', '2010-03-25 17:12:52', '2020-03-18 11:05:41'),
+(4, 'oui', 2, 'John', 'Preum\'s !', '2010-03-27 18:59:49', '2020-03-18 11:05:41'),
+(5, 'oui', 2, 'Maxime', 'Excellente analyse de la situation !\r\nIl y arrivera plus tôt qu\'on ne le pense !', '2010-03-27 22:02:13', '2020-08-31 16:06:14'),
+(17, 'non', 11, 'plop', 'test comment', '2020-07-24 21:27:20', '2020-09-04 15:16:49'),
+(20, 'non', 11, 'Loly', 'je test', '2020-08-28 15:27:27', '2020-09-03 07:27:21');
 
 -- --------------------------------------------------------
 
@@ -95,27 +88,24 @@ VALUES (1, 1, 'M@teo21', 'Un peu court ce billet !', '2010-03-25 16:49:53', '202
 --
 
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user`
-(
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `login` varchar(30) CHARACTER SET utf8 NOT NULL,
-    `password` varchar(250) CHARACTER SET utf8 NOT NULL,
-    `mail` varchar(250) CHARACTER SET utf8 NOT NULL,
-    `role` varchar(30) CHARACTER SET utf8 NOT NULL,
-    `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
-    `updatedAt` datetime NOT NULL DEFAULT current_timestamp(),
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 8 DEFAULT CHARSET = latin1;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(250) CHARACTER SET utf8 NOT NULL,
+  `mail` varchar(250) CHARACTER SET utf8 NOT NULL,
+  `role` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `user`
 --
 
-INSERT INTO `user` (`id`, `login`, `password`, `mail`, `role`, `createdAt`, `updatedAt`)
-VALUES (1, 'Loly', '$argon2id$v=19$m=65536,t=4,p=1$b1c3Mkw1QVIvc1Q0ay5lNw$Va9azRXMlL1sQ0zZgEsOEPUCTRuJz2rDx8xrOOrxbTg',
-        'loly909@hotmail.fr', 'admin', '2020-05-27 12:19:04', '2020-05-27 12:19:04'),
-       (2, 'Plop', '$argon2id$v=19$m=65536,t=4,p=1$TEpkZFJULlBMcU9TOXplYw$K7PeeQQOwSpIpGO4OyZkU7xhwaoZqlmdcQyL0A4q8ps',
-        'plop@hotmail.fr', 'user', '2020-06-03 10:51:25', '2020-06-03 10:51:25');
+INSERT INTO `user` (`id`, `login`, `password`, `mail`, `role`, `createdAt`, `updatedAt`) VALUES
+(1, 'Loly', '$argon2id$v=19$m=65536,t=4,p=1$d0cwdVRER0FvS3dEbU94UA$UjL5NYDe2Od6m10VlfoGFT2E70i38THCZc3Ph/w5Zvk', 'eloise.ruiz.rodriguez@gmail.com', 'admin', '2020-10-01 10:44:47', '2020-10-01 08:44:47'),
+(2, 'Plop', '$argon2id$v=19$m=65536,t=4,p=1$aTVwdTIvOE5ScDNUZTNlcg$xO19hcH6hYnUn6FUzDXfjvBADA5zmN4dDTmnDOnFW00', 'loly909@hotmail.fr', '', '2020-09-19 10:57:45', '2020-09-19 08:57:45');
 
 --
 -- Contraintes pour les tables déchargées
@@ -125,11 +115,11 @@ VALUES (1, 'Loly', '$argon2id$v=19$m=65536,t=4,p=1$b1c3Mkw1QVIvc1Q0ay5lNw$Va9azR
 -- Contraintes pour la table `comment`
 --
 ALTER TABLE `comment`
-    ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`articleId`) REFERENCES `article` (`id`) ON DELETE CASCADE,
-    ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`articleId`) REFERENCES `article` (`id`) ON DELETE CASCADE,
-    ADD CONSTRAINT `comment_ibfk_3` FOREIGN KEY (`articleId`) REFERENCES `article` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`articleId`) REFERENCES `article` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`articleId`) REFERENCES `article` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comment_ibfk_3` FOREIGN KEY (`articleId`) REFERENCES `article` (`id`) ON DELETE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
